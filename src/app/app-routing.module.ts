8489bc90aa4e89a96eb4a2d82a7762d0cb7ccfd7
Jose Mellado <jose.mellado@usach.cl>
2020-06-25 02:24:36 -0400

    avance mockup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {DesignComponent} from './views/design/design.component';
+import {DesignPointsComponent} from './views/design-points/design-points.component';
+import {DesignLevelsComponent} from './views/design-levels/design-levels.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'design', component: DesignComponent},
+  { path: 'design/points', component: DesignPointsComponent},
+  { path: 'design/levels', component:  DesignLevelsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import { DesignComponent } from './views/design/design.component';
+import { DesignPointsComponent } from './views/design-points/design-points.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import { DesignLevelsComponent } from './views/design-levels/design-levels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DesignComponent,
+    DesignPointsComponent,
+    DesignLevelsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
